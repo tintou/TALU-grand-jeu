873e4ca3371a2d1e9c7168b25942a8e321a813bd
Corentin Noël <corentin@elementary.io>
2018-05-07 00:34:33 +0100

    Première itération

---
+++ generation.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+xelatex -output-directory=result ah-soh-koh.tex
+xelatex -output-directory=result emballage-outils.tex
+xelatex -output-directory=result labyrinthe-aveugle.tex
+xelatex -output-directory=result baton-helium.tex
+xelatex -output-directory=result cercle.tex
+xelatex -output-directory=result course-relai.tex
+xelatex -output-directory=result kim.tex
+xelatex -output-directory=result kim-liste.tex
+xelatex -output-directory=result attrape-billes.tex
+xelatex -output-directory=result verre-qui-boit.tex
+xelatex -output-directory=result parcours-aveugle.tex
+xelatex -output-directory=result times-up.tex
