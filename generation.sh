3b6e0ad52bd1b659ebf4051c9012daf5f30b5fa5
Corentin Noël <corentin@elementary.io>
2018-06-05 10:28:39 +0100

    New version

--- generation.sh
+++ generation.sh
@@ -3,17 +3,30 @@
 HTML_PAGE="""<!doctype html>
 <html>
 <head>
+    <meta charset=\"utf-8\">
     <title>TALU Grand Jeu</title>
 </head>
 
-<body>"""
+<body>\n"""
 mkdir result | true
-for f in *.tex
+mkdir docs | true
+
+for f in *-*-*/*.tex
 do
+    if [ ! -f "$f"] || [ ! -s "$f" ]
+    then
+      echo "Empty file: $f"
+      continue
+    fi
     echo "Generating: $f"
-    lualatex --output-directory=result $f
+    out_dir=result/$(dirname "$f")
+    docs_dir=docs/$(dirname "$f")
+    mkdir "$out_dir" | true
+    mkdir "$docs_dir" | true
+    lualatex --output-directory="$out_dir" "$f" --interaction=errorstopmode
     fpdf=$(echo "$f"|sed -e 's/.tex/.pdf/g')
-    HTML_PAGE=$HTML_PAGE"<a href=\"$fpdf\">$fpdf</a><br>"
+    cp "result/$fpdf" "$docs_dir/"
+    HTML_PAGE=$HTML_PAGE"<a href=\"$fpdf\">$fpdf</a><br>\n"
 done
 
 HTML_PAGE=$HTML_PAGE"""
@@ -22,6 +35,4 @@ HTML_PAGE=$HTML_PAGE"""
 </html>"""
 
 # publish the website
-mkdir docs | true
 echo "$HTML_PAGE" > "docs/index.html"
-cp result/*.pdf docs/
